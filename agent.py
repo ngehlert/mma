001506e55e141c2545015aced33759fa07404a64
Nicolas Gehlert <nicolas.gehlert@active-agent.com>
2025-09-26 14:50:59 +0200

    added book and movie recommender

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 from strands import Agent, tool
 from strands.models import BedrockModel
 from camera_tool import camera, analyze_image
+from strands_tools.browser import AgentCoreBrowser
 
 # Create a BedrockModel
 bedrock_model = BedrockModel(
@@ -8,6 +9,7 @@ bedrock_model = BedrockModel(
     region_name="us-west-2",
     temperature=0.3,
 )
+agent_core_browser = AgentCoreBrowser(region="us-west-2")
 
 @tool
 def camera_agent(prompt=None):
@@ -26,16 +28,49 @@ def activity_recommender(prompt=None):
         model=bedrock_model,
         tools=[],
         system_prompt="""You are a specialized activity recommendation assistant.
-            Provide personalized activity suggestions based on user group size and weather."""
+            Provide personalized activity suggestions based on user group size.
+            Assume that the weather is rainy and the time is afternoon.
+            Keep the answer as short as possible and just list 3 activities."""
     )
+
+    return activity_agent(prompt)
+
+@tool
+def book_recommender(prompt=None):
+    book_agent = Agent(
+        model=bedrock_model,
+        tools=[agent_core_browser.browser],
+        system_prompt="""You are a specialized book recommendation assistant.
+            Provide personalized book suggestion. Assume that the user likes science fiction and fantasy books.
+            Take suggestions from amazon.de. Keep the answer as short as possible and just list 3 books.""",
+    )
+
+    return book_agent(prompt)
+
+@tool
+def movie_recommender(prompt=None):
+    movie_agent = Agent(
+        model=bedrock_model,
+        tools=[agent_core_browser.browser],
+        system_prompt="""You are a specialized movie recommendation assistant.
+        Use https://friedrichsbau-kino.de/programm-tickets/friedrichsbau/#default to find the movies for today in theaters in Freiburg, Germany.
+        Provide a concise list of the movies. Add the FSK and Genre if available. Try to keep it to 1 line per movie."""
+    )
+
+    return movie_agent(prompt)
 
 agent = Agent(
     model=bedrock_model,
-    tools=[camera_agent],
+    tools=[camera_agent, activity_recommender, book_recommender, movie_recommender],
     system_prompt="""You are a general activity recommendation assistant.
-            Based on the number of people and other factors, like weather and time of day, recommend suitable activities."""
+        When asked for activity suggestions based on group size, first use the camera_agent tool to determine the number of people present.
+        Then, use the activity_recommender tool.
+        If books are in the suggested activities, use the book_recommender to provide recommendations.
+        If movies are in the suggested activities, use the movie_recommender to provide recommendations.
+        Don't add any extra information, just provide the recommendations provided by the tools.""",
 )
 
-agent("How many people are in front of the pc?")
+# agent("How many people are in front of the pc?")
+agent("What can we do")
 
 
